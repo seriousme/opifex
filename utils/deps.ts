23685dee863815b1645f054e70a996be53ee2b14
Hans Klunder <hans.klunder@gmail.com>
2023-08-05 18:53:02 +0200

    Updated std@0.197

--- utils/deps.ts
+++ utils/deps.ts
@@ -1,3 +1,3 @@
-export { parse } from "https://deno.land/std@0.196.0/flags/mod.ts";
-export { assert } from "https://deno.land/std@0.196.0/assert/mod.ts";
-export { BufReader } from "https://deno.land/std@0.196.0/io/mod.ts";
+export { parse } from "https://deno.land/std@0.197.0/flags/mod.ts";
+export { assert } from "https://deno.land/std@0.197.0/assert/mod.ts";
+export { BufReader } from "https://deno.land/std@0.197.0/io/mod.ts";
