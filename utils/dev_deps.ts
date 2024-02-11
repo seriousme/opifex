6cfc339dae3a9e046ecdece9e9c3034693f9b8b6
Hans Klunder <hans.klunder@gmail.com>
2024-02-11 21:20:38 +0100

    Update to std@0.215

--- utils/dev_deps.ts
+++ utils/dev_deps.ts
@@ -2,6 +2,6 @@ export {
   assertArrayIncludes,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.197.0/assert/mod.ts";
-export { Buffer } from "https://deno.land/std@0.197.0/io/buffer.ts";
-export { readerFromIterable } from "https://deno.land/std@0.197.0/streams/mod.ts";
+} from "https://deno.land/std@0.215.0/assert/mod.ts";
+export { Buffer } from "https://deno.land/std@0.215.0/io/buffer.ts";
+export { readerFromIterable } from "https://deno.land/std@0.215.0/streams/mod.ts";
