120e77b5a064a8d03d7526c991fa6c9e90a0cc33
Hans Klunder <hans.klunder@gmail.com>
2024-03-07 06:48:31 +0100

    remove foreach

--- server/handlers/handleUnsubscribe.ts
+++ server/handlers/handleUnsubscribe.ts
@@ -5,11 +5,11 @@ export async function handleUnsubscribe(
   ctx: Context,
   packet: UnsubscribePacket,
 ): Promise<void> {
-  packet.topicFilters.forEach((topic) => {
+  for (const topic of packet.topicFilters) {
     if (ctx.store) {
       ctx.persistence.unsubscribe(ctx.store, topic);
     }
-  });
+  }
   await ctx.send({
     type: PacketType.unsuback,
     id: packet.id,
