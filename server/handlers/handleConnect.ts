94e2d8a3b7b769dc92dd6fe9416fa238fb32ebd7
Hans Klunder <hans.klunder@gmail.com>
2025-09-12 10:49:42 +0200

    restrict bridgeMode in server

--- server/handlers/handleConnect.ts
+++ server/handlers/handleConnect.ts
@@ -37,6 +37,9 @@ function validateConnect(
   if (packet.protocolLevel !== 4) {
     return AuthenticationResult.unacceptableProtocol;
   }
+  if (packet.bridgeMode === true) {
+    return AuthenticationResult.unacceptableProtocol;
+  }
   return isAuthenticated(ctx, packet);
 }
 
