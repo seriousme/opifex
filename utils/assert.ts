dff0c6867880c8dd84a174d4f48114c98a0ba45a
Hans Klunder <hans.klunder@gmail.com>
2025-09-21 21:50:11 +0200

    chore: add more tests (#41)
    * chore: add more tests
    
    * fix lint

--- utils/assert.ts
+++ utils/assert.ts
@@ -12,21 +12,3 @@ export function assert(expr: unknown, msg = ""): asserts expr {
     throw new AssertionError(msg);
   }
 }
-
-/** An array-like object (`Array`, `Uint8Array`, `NodeList`, etc.) that is not a string */
-export type ArrayLikeArg<T> = ArrayLike<T> & object;
-
-export function assertArrayIncludes<T>(
-  actual: ArrayLikeArg<T>,
-  expected: ArrayLikeArg<T>,
-  msg?: string,
-) {
-  const actualArr = Array.from(actual);
-  const expectedArr = Array.from(expected);
-  for (const item of expectedArr) {
-    assert(
-      actualArr.includes(item),
-      msg + `\nExpected: ${expectedArr}\nActual: ${actualArr}`,
-    );
-  }
-}
