4b963e965299301f1d9c5a4df3b1e69087fa93de
Hans Klunder <hans.klunder@gmail.com>
2025-10-12 18:46:16 +0200

    chore(dev): refactor benchmarks

---
+++ benchmarks/receiver.ts
@@ -0,0 +1,35 @@
+import { exit } from "node:process";
+import { importClientClass } from "./utils.ts";
+
+const { TcpClient } = await importClientClass();
+const client = new TcpClient();
+client.onError = (err) => {
+  console.log("client error", err);
+  exit(1)
+};
+
+await client.connect({
+  url: new URL("mqtt://localhost:1883"),
+});
+
+await client.subscribe({ subscriptions: [{ topicFilter: "test", qos: 0 }] });
+
+let counter = 0;
+const interval = 5000;
+
+function count() {
+  console.log("received/s", (counter / interval) * 1000);
+  counter = 0;
+}
+
+setInterval(count, interval);
+
+// // Consume using a callback
+// client.onPacket = async (packet) => {
+//   counter++;
+// };
+
+// Consume using an AsyncIterator
+for await (const _ of client.messages()) {
+  counter++;
+}
--- benchmarks/utils.ts
+++ benchmarks/utils.ts
@@ -1,32 +1,11 @@
-import type { Client } from "../client/client.ts";
-
-export type ClientClass = new () => Client;
-
-export type Main = (clientClass: ClientClass) => void | Promise<void>;
-
+export async function importClientClass() {
 // @ts-ignore: the Deno global variable is not defined in @types/node,
 //             therefore we need to ignore this error because this file
 //             is meant to be used in both environments.
 const isDeno = typeof Deno !== "undefined";
-
-// @ts-ignore: the Deno global variable is not defined in @types/node,
-//             therefore we need to ignore this error because this file
-//             is meant to be used in both environments.
-// deno-lint-ignore no-process-global
-const exit = isDeno ? Deno.exit : process.exit;
-
-const importClientClass = async (): Promise<ClientClass> => {
   return isDeno
-    ? (await import("../deno/tcpClient.ts")).TcpClient
-    : (await import("../node/tcpClient.ts")).TcpClient;
+    ? (await import("../deno/tcpClient.ts"))
+    : (await import("../node/tcpClient.ts"));
 };
 
-export const loader = async (main: Main) => {
-  const TcpClient = await importClientClass();
-  try {
-    await main(TcpClient);
-  } catch (err) {
-    console.error(err);
-    exit(1);
-  }
-};
+
