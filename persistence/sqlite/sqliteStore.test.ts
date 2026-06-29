9edb43bb827da60c4bb13a63193e6c11a53430cd
Hans Klunder <hans.klunder@gmail.com>
2026-06-29 15:01:55 +0200

    merge sqliteStoreCore into sqliteStore

--- persistence/sqlite/sqliteStore.test.ts
+++ persistence/sqlite/sqliteStore.test.ts
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import { test } from "node:test";
 import { initializeDatabase } from "./sqliteDatabase.ts";
-import { SQLiteStore } from "./sqliteStoreCore.ts";
+import { SQLiteStore } from "./sqliteStore.ts";
 
 test("SQLiteStore generates unique packet IDs", () => {
   const db = initializeDatabase(":memory:");
