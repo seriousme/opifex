95db8bce6f0023bcfbe4a3fb01ffc5a6e5f01409
Hans Klunder <hans.klunder@gmail.com>
2026-08-13 15:04:18 +0200

    feat(mqttPacket): error on properties that cannot contain 0 but do contain 0

--- mqttPacket/decoder.ts
+++ mqttPacket/decoder.ts
@@ -14,6 +14,7 @@ import type {
   ValidPropertyNumber,
 } from "./Properties.ts";
 import {
+  nonZeroProperties,
   propertyByNumber,
   PropertyByPropertySetType,
   propertyKind,
@@ -264,6 +265,11 @@ export class Decoder {
       }
 
       const value = this.getProperty(id);
+      if (nonZeroProperties.includes(id)) {
+        if (value === 0) {
+          throw new DecoderError(`Property ${label} cannot contain 0`);
+        }
+      }
 
       // deno-lint-ignore no-explicit-any
       if ((properties as any)[label] !== undefined) {
