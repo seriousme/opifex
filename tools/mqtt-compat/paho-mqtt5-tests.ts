601a7b0a0060d4e7675a54ba38920058aa5bd850
Hans Klunder <hans.klunder@gmail.com>
2026-08-14 06:48:05 +0200

    update paho5 test

--- tools/mqtt-compat/paho-mqtt5-tests.ts
+++ tools/mqtt-compat/paho-mqtt5-tests.ts
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import { test } from "node:test";
 import type { AnyPacket, QoS } from "../../server/deps.ts";
-import { logger, LogLevel, MQTTLevel, PacketType } from "../../server/deps.ts";
+import { logger, LogLevel, PacketType } from "../../server/deps.ts";
 import {
   addMockClient,
   connect5,
