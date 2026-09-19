78f203317d5d554bcfb6483ee772d2880b25a6e8
Hans Klunder <hans.klunder@gmail.com>
2026-09-19 16:12:53 +0200

    update benchmarking

--- tools/benchmarks/sender.ts
+++ tools/benchmarks/sender.ts
@@ -1,6 +1,9 @@
 import { exit } from "node:process";
 import { runAsap } from "../../dev_utils/timers.ts";
-import { importClientClass } from "./utils.ts";
+import { getArgs, importClientClass } from "./utils.ts";
+import { logger } from "../../utils/mod.ts";
+import type { TLogLevel } from "../../utils/logger.ts";
+import type { QoS } from "../../client/deps.ts";
 
 const { TcpClient } = await importClientClass();
 const client = new TcpClient();
@@ -9,24 +12,37 @@ client.onError = (err: Error) => {
   exit(1);
 };
 
-await client.connect({
-  url: new URL("mqtt://localhost:1883"),
-});
+export async function runSender(
+  level: TLogLevel,
+  url: string,
+  topic: string,
+  qos: QoS,
+) {
+  logger.level(level);
+  await client.connect({
+    url: new URL(url),
+  });
 
-const payload = new TextEncoder().encode("payload");
-let sent = 0;
-const interval = 5000;
+  const payload = new TextEncoder().encode("payload");
+  let sent = 0;
+  const interval = 5000;
 
-const loop = () => {
-  sent++;
-  client.publish({ topic: "test", payload })
-    .then(() => runAsap(loop));
-};
+  const loop = () => {
+    sent++;
+    client.publish({ topic, qos, payload })
+      .then(() => runAsap(loop));
+  };
 
-function count() {
-  console.log("sent/s", (sent / interval) * 1000);
-  sent = 0;
+  function count() {
+    console.log("sent/s", (sent / interval) * 1000);
+    sent = 0;
+  }
+
+  setInterval(count, interval);
+  loop();
 }
 
-setInterval(count, interval);
-loop();
+if (import.meta.main) {
+  const { level, url, topic, qos } = getArgs();
+  runSender(level, url, topic, qos);
+}
--- tools/benchmarks/utils.ts
+++ tools/benchmarks/utils.ts
@@ -1,3 +1,7 @@
+import { parseArgs } from "node:util";
+import { LogLevel } from "../../utils/mod.ts";
+import type { QoS } from "../../client/mod.ts";
+
 export async function importClientClass() {
   // @ts-ignore: the Deno global variable is not defined in @types/node,
   //             therefore we need to ignore this error because this file
@@ -6,4 +10,22 @@ export async function importClientClass() {
   return isDeno
     ? (await import("../../deno/tcpClient.ts"))
     : (await import("../../node/tcpClient.ts"));
+}
+
+export function getArgs() {
+  const benchMarkOpts = {
+    url: { type: "string", short: "u", default: "mqtt://localhost:1883" },
+    topic: { type: "string", short: "t", default: "benchmark" },
+    qos: { type: "string", short: "q", default: "0" },
+    logLevel: { type: "string", short: "l", default: "error" },
+  } as const;
+
+  const { values } = parseArgs({ options: benchMarkOpts });
+  const url = values.url as string;
+  const topic = values.topic as string;
+  const qos = Number(values.qos ?? 0) as QoS;
+  const logLevelKey = (values.logLevel ?? "error") as keyof typeof LogLevel;
+  const level = LogLevel[logLevelKey] ?? LogLevel.error;
+
+  return { url, topic, qos, level };
 }
